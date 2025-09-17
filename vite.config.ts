37c2d5c798953cf98adb96bbe030976038478947
Hannes Axelsson <157214175+Softhe@users.noreply.github.com>
2025-09-17 22:42:44 +0200

    Set base path for non-serve commands

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+ if (command !== 'serve') {
+  config.base = '/delli-crosshair-shine/'
+  }
 }));
