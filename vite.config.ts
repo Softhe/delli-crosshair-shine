7f095274bbdba4806ea170d347e9efd09d8a815b
Hannes Axelsson <157214175+Softhe@users.noreply.github.com>
2025-09-17 22:27:26 +0200

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    base: "/delli-crosshair-shine/",
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
