aaa4d3bb6a5671dd006640e552c9d3089fed67de
Hannes Axelsson <157214175+Softhe@users.noreply.github.com>
2025-09-22 20:54:14 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/delli-crosshair-shine/">
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/delli-crosshair-shine/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
