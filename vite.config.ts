71b7e1f6597e38f29cc0d19280f0704c90764abd
Softhe <suport.softhe@gmail.com>
2025-09-22 23:33:51 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
   const isServe = command === "serve";
 
   return {
-    base: isServe ? "/" : "/delli-crosshair-shine/",
+    base: "/",
     plugins: [react(), isDev && componentTagger()].filter(Boolean),
     resolve: {
       alias: {
