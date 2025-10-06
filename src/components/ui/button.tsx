d44bfd34d26696da935c478c8cf7516e7eed51da
Hannes Axelsson <157214175+Softhe@users.noreply.github.com>
2025-10-06 02:42:43 +0200

    asd

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -51,10 +51,3 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
-      />
-    )
-  }
-)
-Button.displayName = "Button"
-
-export { Button, buttonVariants }
